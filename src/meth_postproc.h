c517e0bb6f7799de0a5045d3899d04733d6d1d27
Nils Homer <nilshomer@gmail.com>
2026-04-18 12:47:05 -0700

    feat(meth): add CIGAR longest-M helper with unit tests

--- src/meth_postproc.h
+++ src/meth_postproc.h
@@ -6,6 +6,9 @@
 extern "C" {
 #endif
 
+/* Longest run of M, =, or X CIGAR ops. Returns 0 on "*" or parse error. */
+int meth_longest_m(const char *cigar);
+
 /* Main entry. argc/argv start at "meth-postproc" (argv[0] == "meth-postproc"). */
 int meth_postproc_main(int argc, char *argv[]);
 
